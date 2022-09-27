6c3e3e0af44e7e7d6716c828e11e04d0e17546fa
Sandeep-Balli <sandeepballi.tech@gmail.com>
2022-09-27 13:29:22 +0530

    Client side development done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Editor from './component/Editor';
+
+function App() {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+}
+
+export default App;
